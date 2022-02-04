4886578d7114fa0fae6c96e50675bc389b08b15e
infamoushotdog <n.krapotkin@gmail.com>
2022-02-04 19:41:15 +0500

    Initial коммит

---
+++ buildSrc/src/main/java/Libraries.kt
@@ -0,0 +1,6 @@
+object AndroidXLibs {
+
+    const val composeUi = "androidx.compose.ui:ui:${Versions.compose}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.compose}"
+    const val composeUiToolingPreview = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,4 @@
+object Versions {
+
+    const val compose = "1.0.5"
+}
